5e4d8fafd02d56986c9bee75ec1831865f215d59
Amy <amysnodgrass927@gmail.com>
2024-11-07 12:06:43 -0500

    class activity

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,8 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
+import android.content.Intent.ACTION_SEND
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -12,10 +15,12 @@ class MainActivity : AppCompatActivity() {
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
+        val intent = Intent(this, MainActivity::class.java)
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val text = Uri.parse(editText.toString())
+            startActivity(intent.putExtra(ACTION_SEND, text))
         }
     }
 }
